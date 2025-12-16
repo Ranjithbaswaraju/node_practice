bd3c5fe1b80e21b70eddd75be5940b673e1e8912
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-12-16 15:17:19 +0530

    updated

--- app.js
+++ app.js
@@ -3,3 +3,65 @@ const colors=require("colors")
 console.log('hi'.yellow)
 console.log("bye".bgYellow)
 console.log("NARENDRA".bgBlue)
+
+
+
+//
+
+// const fs=require('fs');
+// const data=fs.writeFileSync("a.txt","hello",(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("data added")
+//     }
+// })
+
+
+// write file synchronously
+
+// const fs=require('fs');
+// const data=fs.writeFileSync("a.txt","bye")
+// console.log(data)
+
+
+
+//readFile sync
+
+// const fs=require('fs')
+// fs.readFile("a.txt",'utf-8',(err,info)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log(info)
+//     }
+// })
+
+// reading file synchronos way
+
+// const fs=require('fs')
+// const data=fs.readFileSync("a.txt",'utf-8')
+// console.log(data)
+
+// appending data
+
+// const fs=require('fs')
+// fs.appendFile("a.txt","ranjith",(err,info)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log(info)
+//     }
+// })
+
+// const fs=require('fs')
+// const data=fs.existsSync("a.txt")
+// console.log(data)
+
+
+const fs=require('fs')
+const data=fs.unlinkSync("a.txt")
+console.log(data)
